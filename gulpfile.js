c68d6ddafca6f31cf3422bebfb4b1c13c8c2fabd
Nikita <nikita.show@mail.ru>
2020-05-04 16:05:01 +0300

    Create gulp: mincss, rename and browser-sync

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
+
+gulp.task ('hello', function (done) {
+  console.log ('Hello world');
+  done();
+});
+
+// Static server
+gulp.task('browser-sync', function() {
+  browserSync.init({
+      server: {
+          baseDir: "./"
+      }
+  });
+  gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+gulp.task('minify-css', function() {
+  return gulp.src('css/*.css')
+      .pipe(cleanCSS({compatibility: 'ie8'}))
+      .pipe(rename(function (path) {
+        path.extname =".min.css";
+      }))
+      .pipe(gulp.dest('css'))
+});
